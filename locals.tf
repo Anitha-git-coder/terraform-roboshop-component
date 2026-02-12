a8d0c62857438134483b66493cacdd30a4e88867
Anitha <anitha.pinipe14@gmail.com>
2026-02-12 14:36:51 +0530

    terraform

--- locals.tf
+++ locals.tf
@@ -2,6 +2,8 @@ locals {
   ami_id             = data.aws_ami.joindevops.id
   sg_id    = data.aws_ssm_parameter.sg_id.value
   private_subnet_id  = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
+  private_subnet_ids=data.aws_ssm_parameter.private_subnet_ids.value
+
   common_name_suffix = "${var.project_name}-${var.environment}" #--roboshop-dev 
   tg_port = "${var.component}" == "frontend" ? 80 :8080 
   vpc_id=data.aws_ssm_parameter.vpc_id.value
